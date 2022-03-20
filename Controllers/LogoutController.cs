4f1714472635380c1b9fce1a749f004ae441d6a7
unknown <Yihao Zheng>
2022-03-20 17:07:23 -0400

    Add register, logout functionality

---
+++ Controllers/LogoutController.cs
@@ -0,0 +1,24 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using CSI5112BackEndApi.Models;
+using CSI5112BackEndApi.Services;
+
+namespace CSI5112BackEndApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LogoutController: ControllerBase
+{
+    // Supposed to delete session
+    [HttpPost]
+    public IActionResult Logout()
+    {
+        return Ok(new 
+        {
+            message = "You have logged out"
+        });
+    }
+}
---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Mvc;
+using CSI5112BackEndApi.Models;
+using CSI5112BackEndApi.Services;
+
+namespace CSI5112BackEndApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RegisterController: ControllerBase
+{
+    private readonly CustomersService _CustomersService;
+    private readonly MerchantsService _MerchantsService;
+
+    public RegisterController(CustomersService customersService, MerchantsService merchantsService)
+    {
+        _CustomersService = customersService;
+        _MerchantsService = merchantsService;
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+    {
+         
+        if (!request.isMerchant) {
+            List<Customer> customers = _CustomersService.GetCustomerByUsername(request.username).Result;
+            if (customers.Count != 0) {
+                return BadRequest(new
+                {
+                    error = "Username has been used by other user"
+                });
+            }
+            Customer c = new Customer{
+                customer_id = request.id,
+                username = request.username,
+                password = request.password,
+                phone = request.phone,
+                first_name = request.first_name,
+                last_name = request.last_name,
+                email = request.email
+            };
+
+            await _CustomersService.CreateNewCustomer(c);
+            return Ok(new 
+                {
+                    success = "You have registered in our application as customer"
+                });
+
+
+        } else {
+            List<Merchant> merchants = _MerchantsService.GetMerchantByUsername(request.username).Result;
+            if (merchants.Count != 0) {
+                return BadRequest(new
+                {
+                    error = "Username has been used by other user"
+                });
+            }
+            Merchant m = new Merchant{
+                merchant_id = request.id,
+                username = request.username,
+                password = request.password,
+                phone = request.phone,
+                first_name = request.first_name,
+                last_name = request.last_name,
+                email = request.email
+            };
+
+            await _MerchantsService.CreateNewMerchant(m);
+            return Ok(new 
+                {
+                    success = "You have registered in our application as merchant"
+                });
+        }
+    }
+}
